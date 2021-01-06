c8be34dfae096f9c1bd7a36a473005d37eba0ef0
RubenEngel <66388962+RubenEngel@users.noreply.github.com>
2021-01-06 12:02:13 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-io = SocketIO(app)
 
-@app.route('/')
-def index():
-    return 'hello world'
-
-if __name__ == '__main__':
-    io.run(app, host="0.0.0.0", port=5000)
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
