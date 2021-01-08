5270023fc6066fa5861f0c1b4d33a337c313c037
RubenEngel <ruben.engel98@gmail.com>
2021-01-08 13:48:17 +0000

    File restructure

--- server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import time
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-
-@app.route('/api/time')
-def get_current_time():
-    return {'time': time.time()}
