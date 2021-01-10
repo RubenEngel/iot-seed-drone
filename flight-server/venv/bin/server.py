e668df610e05881e65c6e9c61bb493e040fa6442
RubenEngel <66388962+RubenEngel@users.noreply.github.com>
2021-01-10 14:02:59 +0000

    Update server.py

--- flight-server/venv/bin/server.py
+++ flight-server/venv/bin/server.py
@@ -10,8 +10,8 @@ def get_current_time():
 
 @app.route('/api/params', methods = ['POST'])
 def get_flight_params():
-    dropHeight = request.args.get(dropHeight)
-    dropColumns = request.args.get(dropColumns)
-    dropRows = request.args.get(dropRows)
+    dropHeight = request.args.get('dropHeight')
+    dropColumns = request.args.get('dropColumns')
+    dropRows = request.args.get('dropRows')
     return jsonify(dropHeight=dropHeight, dropColumns=dropColumns, dropRows=dropRows)
     
