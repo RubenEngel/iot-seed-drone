30736c9756bac9520cc0f710afb00ae45a024090
RubenEngel <66388962+RubenEngel@users.noreply.github.com>
2021-01-10 15:41:49 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,7 +54,8 @@ function App() {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(flightParams)
-          }).then(res => res.json()).then(data => console.log(data))
+          }).then(res => console.log(res.ok))
+          .catch(err => console.log(err))
         }
         }
 
