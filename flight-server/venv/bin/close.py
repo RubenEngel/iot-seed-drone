8a0f1addd8f7de752d2dde460760d736ac720404
RubenEngel <66388962+RubenEngel@users.noreply.github.com>
2021-01-20 18:13:04 +0000

    Cut down open and close files

--- flight-server/venv/bin/close.py
+++ flight-server/venv/bin/close.py
@@ -1,12 +1,7 @@
 import serial
-import time
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
+    ser.write(b'0')
 
-    while True:
-        ser.write(b'0')
-        # line = ser.readline().decode('utf-8').rstrip()
-        # print(line)
-        # time.sleep(1)
--- flight-server/venv/bin/open.py
+++ flight-server/venv/bin/open.py
@@ -1,12 +1,6 @@
 import serial
-import time
 
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
-
-    while True:
-        ser.write(b'1')
-        # line = ser.readline().decode('utf-8').rstrip()
-        # print(line)
-        # time.sleep(1)
+    ser.write(b'1')
