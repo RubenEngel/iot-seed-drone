53a3b215f46b69b9bfaf209127c165cd4886acea
RubenEngel <ruben.engel98@gmail.com>
2021-01-27 12:54:18 +0000

    sandbox

--- flight-server/flight-scripts/sandbox.py
+++ flight-server/flight-scripts/sandbox.py
@@ -1,9 +1,1 @@
-import subprocess
-
-drop_height = 2.5
-drop_spacing = 3.5
-drop_columns = 3
-drop_rows = 3
-
-subprocess.Popen(['/usr/bin/python', '/home/dronedojo/iot-seed-drone/flight-server/flight-scripts/basic_mission.py', '--connect', '127.0.0.1:14550',\
-        '--height', str(drop_height), '--spacing', str(drop_spacing), '--columns', str(drop_columns), '--rows', str(drop_rows)])
+print({'Velocity': 6, ''})
