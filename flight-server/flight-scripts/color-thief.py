bbc3cb21a8c324daf489776fd0655c6dc7e17d05
RubenEngel <66388962+RubenEngel@users.noreply.github.com>
2021-03-10 19:09:46 +0000

    Update color-thief.py

--- flight-server/flight-scripts/color-thief.py
+++ flight-server/flight-scripts/color-thief.py
@@ -1,16 +1,22 @@
 from colorthief import ColorThief
+from picamera import PiCamera
 
-color_thief = ColorThief('/Users/rubenengel/GitHub/iot-seed-drone/flight-server/flight-scripts/Screen-Shot-2021-01-28-at-19.06.21.png')
+camera = PiCamera()
+
+camera.resolution = (500, 500)
+
+camera.capture('/current_location.jpg')
+
+color_thief = ColorThief('/current_location.jpg')
+
 # get the dominant color
 '''
 The number determines how many pixels are skipped before the next one is sampled. 
 We rarely need to sample every single pixel in the image to get good results. 
 The bigger the number, the faster a value will be returned.
 '''
-dominant_color = color_thief.get_color(quality=5)
+dominant_color = color_thief.get_color(quality=1)
 # build a color palette
-palette = color_thief.get_palette(color_count=6)
-
-
+palette = color_thief.get_palette(color_count=3)
 
 print(dominant_color)
