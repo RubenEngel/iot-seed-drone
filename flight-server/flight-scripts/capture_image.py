09451be1acc309f0a33d367f0afdd099947a2663
RubenEngel <ruben.engel98@gmail.com>
2021-04-03 17:14:26 +0100

    capture image

--- flight-server/flight-scripts/capture_image.py
+++ flight-server/flight-scripts/capture_image.py
@@ -1,7 +1,10 @@
 from picamera import PiCamera
+from datetime import datetime
 
-camera = PiCamera()
+now = datetime.now()
+dt_string = now.strftime("%d%m%Y-%H:%M:%S")
+filename="images/" + dt_string
 
-camera.resolution = (500, 500)
-
-camera.capture('test.jpg')
+with PiCamera() as camera:	
+    camera.resolution = (500, 500)
+    camera.capture(filename)
