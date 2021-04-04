407690ab3f853de0bfaffd7b4f84880655ff0bdf
RubenEngel <ruben.engel98@gmail.com>
2021-04-04 18:10:47 +0100

    changes

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -8,7 +8,7 @@ function Camera() {
         <h2>Camera</h2>
         <div className='mission-log md:text-2xl flex flex-col justify-center'>
             <p className='font-bold mb-2'>Live Stream</p>
-            <div className='w-full test mx-auto mb-8'>
+            <div className='player-wrapper w-full test mx-auto mb-8'>
                 <ReactPlayer 
                 url='127.0.0.1:8080/stream' 
                 width='100%'
