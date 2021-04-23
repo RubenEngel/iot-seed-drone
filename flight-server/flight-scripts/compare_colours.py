f402a71637e5afb29bb618750c4f79a4967ac92a
Ruben Engel <ruben.engel98@gmail.com>
2021-04-23 17:51:35 +0100

    Update compare_colours.py

--- flight-server/flight-scripts/compare_colours.py
+++ flight-server/flight-scripts/compare_colours.py
@@ -5,7 +5,8 @@ from colorthief import ColorThief
 from colormath.color_objects import sRGBColor, LabColor
 from colormath.color_conversions import convert_color
 from colormath.color_diff import delta_e_cie2000
-from datetime import datetime 
+# from datetime import datetime
+import time
 
 parser = argparse.ArgumentParser(description='Specify images to compare')
 
@@ -22,12 +23,12 @@ image1_color_thief = ColorThief(image1)
 image2_color_thief = ColorThief(image2)
 
 # print('Analysing Ground..')
-
+start = time.time()
 # Use color_thief to return an RGB array of dominant colour
 # Image1 3 element array of RGB colours, quality is how many pixels are skipped in analysis (larger number speeds up process)
-image1_colour = image1_color_thief.get_color(quality=10)
+image1_colour = image1_color_thief.get_color(quality=1)
 # Image2 3 element array of RGB colours, quality is how many pixels are skipped in analysis (larger number speeds up process)
-image2_colour = image2_color_thief.get_color(quality=10) 
+image2_colour = image2_color_thief.get_color(quality=1) 
 
 # Convert into color_math RGB object using the RGB array found by color_thief
 image1_rgb = sRGBColor(image1_colour[0], image1_colour[1], image1_colour[2])
@@ -39,8 +40,8 @@ image2_lab = convert_color(image2_rgb, LabColor)
 
 # Difference in the CIE color space
 delta_e = delta_e_cie2000(image1_lab, image2_lab)
-
+end = time.time()
 # print('Analysis Complete')
 
 print('Colour Difference: ' + f'{delta_e:.2f}')
-
+print('{}'.format(end - start))
